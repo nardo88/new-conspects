6ba5b770a7b1d12a408c0c7f9e807ee2c7c566dc
nardo88 <nardo1988@mail.ru>
2021-06-25 12:01:46 +0300

    1

--- conspects/teory/TypeScript/Generics/script.ts
+++ conspects/teory/TypeScript/Generics/script.ts
@@ -1,6 +1,7 @@
 
 
 const arrayOfNumber: Array<number> = [1,1,2,3,5,8,13]
+// либо так
 
 
 
@@ -9,6 +10,19 @@ const arrayOfNumber: Array<number> = [1,1,2,3,5,8,13]
 
  function reverseArray<T> (array: T[]): T[] {
     return array.reverse()
+}
+
+function mergeObjects<T extends object>(a: T, b: T) {
+    return Object.assign({} , a ,b)
+}
+
+const obj = mergeObjects({a: 'Max'}, {b: 32})
+
+
+
+
+function getObjectValue<T extends object , B extends keyof T>(obj: T, key: B){
+    return obj[key]
 }
 
 
