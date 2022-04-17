db3ce1def18fdac5e6b45712885ca3e2a8cffe06
nardo88 <nardo1988@mail.ru>
2022-04-17 11:59:02 +0300

    1

--- conspects/teory/nodejs/file/index.js
+++ conspects/teory/nodejs/file/index.js
@@ -1,24 +1,11 @@
 const fs = require('fs')
-
 const path = require('path')
 
-
-// fs.readdir(__dirname, (err, files) => {
-//     if (!err) {
-//         // получаем файлы в директории
-//         files.forEach(file => {
-//             // Получаем у каждого объект класса Stats
-//             fs.stat(__dirname+'\\'+file, (error, stat) => {
-//                 // Проверяем является ли файл папкой
-//                 if (stat.isDirectory()) {
-//                     console.log(file + " - it's folder")
-//                 } else {
-//                     console.log(file + " - it's file")
-//                 }
-//             })
-//         })
-//     }
-// })
-
-fs.unlinkSync(__dirname +"\\"+ '111.txt')
+fs.appendFile(path.join(__dirname, 'test.txt'), 'some text', (err) => {
+    if(err){
+        console.log(err)
+        return
+    }
+    console.log('success')
+})
 
